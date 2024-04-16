95b9c9aaee4b56aba3932627a766ff40f0d63a5a
nitesh kumar <knitesh706@gmail.com>
2024-04-16 23:57:31 +0530

    day1 and day2

---
+++ first/src/p2/Decision.java
@@ -0,0 +1,15 @@
+package p2;
+
+public class Decision {
+
+//	WAP to test wheter a number is even or not
+	public static void main(String[] args) {
+		int n = 78;
+		int rem = n%2;
+		if(rem ==0) {
+			System.out.println("EVEN NUMBER");
+		}else {
+			System.out.println("ODD NUMBER");
+		}
+	}
+}
---
+++ first/src/package1/D.java
@@ -0,0 +1,29 @@
+package package1;
+
+public class D {
+
+	public static void main(String[] args) {
+//		write a program to get reminder of a division
+//		% --> Moduls Operator
+	  int n1 = 342;
+	  int n2 = 51;
+/*\
+    		342 = 340*10 + 2
+ */ 
+	  /*
+	   342 = 51*6 + 36
+	   */
+	  int result = n1%n2;
+	  System.out.println(result);
+	  
+	  /*
+	   *   1/2
+	   *   
+	   *   56/6765
+	   *   rem = 56
+	   *   
+	   *   1 = 0*2 + 1
+	   */
+		
+	}
+}
